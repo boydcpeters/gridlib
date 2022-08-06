bbd9e704f107a372c34a09b72991b2046c0f609b
Boyd Peters <boyd.c.peters@gmail.com>
2022-08-06 10:55:16 +0200

    Added some extra tests to cover more cases

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -10,12 +10,15 @@ def test__num_decimal_places():
     assert data_utils._num_decimal_places("98.8572") == 4
     assert data_utils._num_decimal_places("3.4120") == 3
     assert data_utils._num_decimal_places("11") == 0
+    assert data_utils._num_decimal_places("0.05") == 2
 
 def test_get_time_sec():
     """Test the function 'get_time_sec()'."""
     assert data_utils.get_time_sec("50ms") == 0.05
     assert data_utils.get_time_sec("4.2s") == 4.2
     assert data_utils.get_time_sec("1150ms") == 1.15
+    assert data_utils.get_time_sec("2s") == 2
+    assert data_utils.get_time_sec("100ms") == 0.1
     
     with pytest.raises(ValueError):
         data_utils.get_time_sec("zems")
@@ -33,6 +36,8 @@ def test__fmt_t_str_key():
     assert data_utils._fmt_t_str_key("50ms") == "0.05s"
     assert data_utils._fmt_t_str_key("4.2s") == "4.2s"
     assert data_utils._fmt_t_str_key("1150ms") == "1.15s"
+    assert data_utils._fmt_t_str_key("2s") == "2s"
+    assert data_utils._fmt_t_str_key("100ms") == "0.1s"
     
     with pytest.raises(ValueError):
         data_utils._fmt_t_str_key("zems")
