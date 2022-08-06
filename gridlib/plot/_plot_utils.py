4e965fc7829fc6b12459d12a12f4a363354387f6
Boyd Peters <boyd.c.peters@gmail.com>
2022-08-06 10:58:50 +0200

    Added a function

--- gridlib/plot/_plot_utils.py
+++ gridlib/plot/_plot_utils.py
@@ -1,3 +1,11 @@
 """
 Module with utility functions required for the plotting.
 """
+
+from .. import data_utils
+
+
+def _fmt_t_str_plot(t_str):
+    """Function returns a formatted string which can be plotted inside figures."""
+    t_s, d_places = data_utils.get_time_sec(t_str, return_num_decimals=True)
+    return rf"${t_s:.{d_places}f}\,\mathrm{{s}}$"
