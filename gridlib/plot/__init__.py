aaeb87c3256095daf8fcc7820180ecd2bcd674bf
Boyd Peters <boyd.c.peters@gmail.com>
2022-08-06 12:30:39 +0200

    Added functions that are accessible

--- gridlib/plot/__init__.py
+++ gridlib/plot/__init__.py
@@ -1,4 +1,5 @@
 from .spectrum import event_spectrum, state_spectrum
+from .survival_function import data_vs_multi_exp, data_vs_grid
 
 
-__all__ = ["event_spectrum", "state_spectrum"]
+# __all__ = ["event_spectrum", "state_spectrum", ""]
