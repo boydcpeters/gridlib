849f83552e90c7a364d2ec8e2e16165ecc26eb89
Boyd Peters <boyd.c.peters@gmail.com>
2022-08-08 11:54:32 +0200

    Updated spectrum plots

--- gridlib/plot/spectrum.py
+++ gridlib/plot/spectrum.py
@@ -8,31 +8,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.offsetbox import AnchoredText
 
-# TODO: use kwargs structure here as well
-
 
 def _base_spectrum(
     k: np.ndarray,
     weight: np.ndarray,
+    scale: str = "log",
+    threshold: float = 0.001,
     figsize: Tuple[float, float] = (10, 6),
     xlim: Tuple[float, float] = None,
     ylim: Tuple[float, float] = None,
+    color="#fe9901",
 ):
-
-    # Color and marker size settings
-    color_results = "#fe9901"
-    markersize = 16
 
     fig1, ax1 = plt.subplots(nrows=1, ncols=1, figsize=figsize)
 
     # Plot the results
-    idx = weight >= 0.00001
-    ax1.scatter(k[idx], weight[idx], s=markersize, color=color_results)
+    idx = weight >= threshold
+    ax1.plot(
+        k[idx], weight[idx], linestyle="None", marker="o", markersize=3, color=color
+    )
     ax1.vlines(
-        k[idx], np.zeros(k.shape[0])[idx], weight[idx], linewidth=1, color=color_results
+        k[idx], np.zeros(k.shape[0])[idx], weight[idx], linewidth=0.75, color=color
     )
 
-    ax1.set_xscale("log")
+    if scale == "log":
+        ax1.set_xscale("log")
 
     # Axis limits
     if xlim is not None:
@@ -45,10 +45,13 @@ def _base_spectrum(
 
 def event_spectrum(
     fit_results_grid,
+    scale: str = "log",
+    threshold: float = 0.001,
     figsize: Tuple[float, float] = (10, 6),
     xlim: Tuple[float, float] = None,
     ylim: Tuple[float, float] = None,
     path_save: Union[str, pathlib.Path] = None,
+    color="#fe9901",
 ):
 
     # Create a Path() from path_save if not None and it is a str
@@ -59,7 +62,16 @@ def event_spectrum(
     weight = fit_results_grid["s"]
     bleaching_number = fit_results_grid["a"]
 
-    fig1, ax1 = _base_spectrum(k, weight, figsize=figsize, xlim=xlim, ylim=ylim)
+    fig1, ax1 = _base_spectrum(
+        k,
+        weight,
+        scale=scale,
+        threshold=threshold,
+        figsize=figsize,
+        xlim=xlim,
+        ylim=ylim,
+        color=color,
+    )
 
     # Labels
     ax1.set_xlabel("dissociation rate (1/s)")
@@ -79,10 +91,13 @@ def event_spectrum(
 
 def state_spectrum(
     fit_results_grid,
+    scale: str = "log",
+    threshold: float = 0.001,
     figsize: Tuple[float, float] = (10, 6),
     xlim: Tuple[float, float] = None,
     ylim: Tuple[float, float] = None,
     path_save: Union[str, pathlib.Path] = None,
+    color="#fe9901",
 ):
 
     # Create a Path() from path_save if not None and it is a str
@@ -96,7 +111,16 @@ def state_spectrum(
     state = (1 / k) * weight
     state = state / np.sum(state)  # normalization
 
-    fig1, ax1 = _base_spectrum(k, state, figsize=figsize, xlim=xlim, ylim=ylim)
+    fig1, ax1 = _base_spectrum(
+        k,
+        state,
+        scale=scale,
+        threshold=threshold,
+        figsize=figsize,
+        xlim=xlim,
+        ylim=ylim,
+        color=color,
+    )
 
     # Labels
     ax1.set_xlabel("dissociation rate (1/s)")
@@ -105,7 +129,9 @@ def state_spectrum(
     # add the bleaching number in the plot
     # https://stackoverflow.com/questions/23112903/matplotlib-text-boxes-automatic-position
     anchored_text = AnchoredText(
-        f"a = {bleaching_number:.5f}", loc="center right", frameon=False
+        f"a = {bleaching_number:.5f}",
+        loc="center right",
+        frameon=False,
     )
     ax1.add_artist(anchored_text)
 
