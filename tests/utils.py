06846e22a8e19164e58358320757ab182ad263d8
Boyd Peters <boyd.c.peters@gmail.com>
2022-08-09 19:22:44 +0200

    Improved the function

--- tests/utils.py
+++ tests/utils.py
@@ -1,17 +1,28 @@
+from typing import Dict
 import numpy as np
 
 
-def approx_nested_dict_data(dict1, dict2):
-    """Function tests whether a nested dictionary with the data structure are
-    approximately equal."""
+def approx_nested_dict_data(
+    dict1: Dict[str, Dict[str, np.ndarray]], dict2: Dict[str, Dict[str, np.ndarray]]
+):
+    """Function tests whether two nested dictionaries with the structure:
+    {
+        f"{t_tl}s": {
+            "time": np.ndarray,
+            "value": np.ndarray,
+        },
+        ...
+    }
+    are approximately equal.
+    """
 
     # Check that all keys are the same
     assert dict1.keys() == dict2.keys()
 
     # Now check that all the nested arrays are approximately the same
     for key1 in dict1.keys():
-        for key2 in dict1[key1].keys():
-            np.testing.assert_allclose(dict1[key1][key2], dict2[key1][key2])
+        np.testing.assert_allclose(dict1[key1]["time"], dict2[key1]["time"])
+        np.testing.assert_allclose(dict1[key1]["value"], dict2[key1]["value"])
 
     # If it goes through all the checks return True
     return True
