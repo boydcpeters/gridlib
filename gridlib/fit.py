b3125dfcf6614b26c3d71d47ae5eae73b944b54e
Boyd Peters <boyd.c.peters@gmail.com>
2022-08-09 12:11:12 +0200

    Working on fitting function for multi_exp

--- gridlib/fit.py
+++ gridlib/fit.py
@@ -78,7 +78,8 @@ def fit_grid(parameters, data):
     bnds = [(0, 1) for _ in range(x0.shape[0])]
     bnds.append((lbq, ubq))  # add the bounds for the photobleaching
     if parameters["fit_a"]:
-        x0 = np.concatenate((x0, np.array([0.01], dtype=np.float64)))
+        # The initial kb guess is 2 s^-1 as default
+        x0 = np.concatenate((x0, np.array([2 * t_int], dtype=np.float64)))
 
     elif not parameters["fit_a"]:
         x0 = np.concatenate((x0, np.array([parameters["a_fixed"]])))
@@ -108,10 +109,73 @@ def fit_grid(parameters, data):
     return fit_results
 
 
-def fit_multi_exp(parameters, data):
+def create_grid_multi_exp(grid_vals):
+    pass
+
+
+def fit_multi_exp(parameters, data, n: int = 2):
     """Function to fit a multi-exponential to the provided data and returns the fitted
     parameter results."""
-    pass
+
+    data_processed = data_utils.process_data(data)
+
+    if not data_utils.isvalid_parameters(parameters):
+        raise ValueError("parameters does not contain a valid value")
+
+    t_int = parameters["t_int"]
+
+    k_min = parameters["k_min"]
+    k_max = parameters["k_max"]
+
+    coordinates = []
+    for i in range(n):
+        coordinates.append(np.geomspace(k_min, k_max, num=2, endpoint=True))
+
+    # Create the grid
+    x0_all = np.meshgrid(*coordinates)
+    x0_all = np.reshape(x0_all, (-1, n))
+
+    print(f"Total fits: {x0_all.shape[0]}")
+
+    fit_results_all = np.zeros((x0), dtype=np.float64)
+
+    # Update the x0 array and set the bounds for both the amplitudes and the
+    # photobleaching number
+    lbq = 0  # lower bound photobleaching
+    ubq = 3  # upper bound photobleaching
+
+    bnds = [(0, 1) for _ in range(x0.shape[0])]
+    bnds.append((lbq, ubq))  # add the bounds for the photobleaching
+    if parameters["fit_a"]:
+        x0 = np.concatenate((x0, np.array([0.05], dtype=np.float64)))
+
+    elif not parameters["fit_a"]:
+        x0 = np.concatenate((x0, np.array([parameters["a_fixed"]])))
+        bnds[-1] = (parameters["a_fixed"], parameters["a_fixed"])
+
+    # print(x0.shape)
+    # print(bnds)
+
+    cons = [{"type": "eq", "fun": con_eq}]
+    options = {"maxiter": 1000, "disp": True}
+
+    res = scipy.optimize.minimize(
+        calc.lsqobj_grid,
+        x0,
+        args=(data_processed, k, reg_weight, t_int),
+        method="SLSQP",
+        jac=True,  # jac=True, means the gradient is given by the function
+        bounds=bnds,
+        constraints=cons,
+        options=options,
+    )
+
+    grid_results = {"k": k, "s": res.x[: k.shape[0]], "a": res.x[-1]}
+
+    fit_results = {"grid": grid_results}
+
+    return fit_results
+    scipy.optimize.least_squares()
 
 
 def fit(parameters, data):
