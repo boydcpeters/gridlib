ace79ee8beaeca48a7c3ef86aa824aa10f1964be
Boyd Peters <boyd.c.peters@gmail.com>
2022-08-09 19:49:12 +0200

    Updated docstring

--- tests/utils.py
+++ tests/utils.py
@@ -5,15 +5,34 @@ import numpy as np
 def approx_nested_dict_data(
     dict1: Dict[str, Dict[str, np.ndarray]], dict2: Dict[str, Dict[str, np.ndarray]]
 ):
-    """Function tests whether two nested dictionaries with the structure:
-    {
-        f"{t_tl}s": {
-            "time": np.ndarray,
-            "value": np.ndarray,
-        },
-        ...
-    }
-    are approximately equal.
+    """Function tests whether two data dictionaries are approximately equal.
+
+    Parameters
+    ----------
+    dict1: Dict[str, Dict[str, np.ndarray]]
+        Survival function data for time-lapse conditions with the following data
+        structure:
+            {
+                "t_tl": {
+                    "time": np.ndarray with the time points,
+                    "value": np.ndarray with the survival function values,
+                },
+                ...
+            }
+    dict2: Dict[str, Dict[str, np.ndarray]]
+        Same as dict1.
+
+    Returns
+    -------
+    bool
+        Returns True if it passes all the checks and the data dictionaries are
+        approximately the same.
+
+    Raises
+    ------
+    AssertionError
+        If a check is not passed and thus the dictionaries are not approximately the
+        same.
     """
 
     # Check that all keys are the same
