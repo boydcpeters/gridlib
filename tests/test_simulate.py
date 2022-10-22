2904d45e108b38dbbc63b39252d3ae7b1e55ae49
Boyd Peters <boyd.c.peters@gmail.com>
2022-10-22 20:53:48 +0200

    Updated file

---
+++ tests/test_simulate.py
@@ -0,0 +1,18 @@
+"""Tests for `calc` module."""
+import numpy as np
+import pytest
+
+from gridlib import simulate
+
+# TODO: write tests
+# def test_tl_simulations():
+#     """Test the working of the `tl_simulate_single` function."""
+#     k = np.array([0.005, 0.1, 4.2], dtype=np.float64)
+#     s = np.array([0.04, 0.2, 0.76], dtype=np.float64)
+#     kb = 1.0
+#     t_int = 0.05
+#     t_tl_all = [0.05, 0.2, 1.0, 5.0]  # , 9.0]
+#     N = 10000
+
+#     data = simulate.tl_simulations(k, s, kb, t_int, t_tl_all, N)
+#     print(data.keys())
