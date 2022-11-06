eb06b507ca658d3685e439465c66caf28d9bae83
Boyd Peters <boyd.c.peters@gmail.com>
2022-11-06 21:34:59 +0100

    Actually added the read_track_file_csv function to the io module init file, so it can be called

--- gridlib/io/__init__.py
+++ gridlib/io/__init__.py
@@ -1,3 +1,8 @@
 from .survival_function import read_data_survival_function, write_data_survival_function
+from .tracks import read_track_file_csv
 
-__all__ = ["read_data_survival_function", "write_data_survival_function"]
+__all__ = [
+    "read_data_survival_function",
+    "write_data_survival_function",
+    "read_track_file_csv",
+]
