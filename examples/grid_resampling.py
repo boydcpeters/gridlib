b1e6f80ea509e0e5004924f1e72d5e5a32ce34ef
Boyd Peters <boyd.c.peters@gmail.com>
2022-11-30 18:01:00 +0100

    added if statement to indicate it is main code

--- examples/grid_resampling.py
+++ examples/grid_resampling.py
@@ -5,40 +5,45 @@ import gridlib
 import gridlib.io
 import gridlib.plot
 
-# Load the data
-data = gridlib.io.read_data_survival_function("examples/data/example1.csv")
+# if __name__ == "__main__" is required for the multiprocessing in the
+# resampling_grid function to work.
+if __name__ == "__main__":
+    # Load the data
+    data = gridlib.io.read_data_survival_function("examples/data/example1.csv")
 
-# Set the parameters
-parameters = {
-    "k_min": 10 ** (-3),
-    "k_max": 10**1,
-    "N": 200,
-    "scale": "log",
-    "reg_weight": 0.01,
-    "fit_a": True,
-    "a_fixed": None,
-}
+    # Set the parameters
+    parameters = {
+        "k_min": 10 ** (-3),
+        "k_max": 10**1,
+        "N": 200,
+        "scale": "log",
+        "reg_weight": 0.01,
+        "fit_a": True,
+        "a_fixed": None,
+    }
 
-# Perform the resampling
-fit_result_full, fit_results_resampled = gridlib.resampling_grid(
-    parameters, data, n=10, perc=0.8
-)
+    # Perform the resampling
+    fit_result_full, fit_results_resampled = gridlib.resampling_grid(
+        parameters, data, n=10, perc=0.8
+    )
 
-print(len(fit_result_full), len(fit_results_resampled))
+    print(len(fit_result_full), len(fit_results_resampled))
 
-# Save the the resampled data
-path_save = "examples/data/example1_resampling.mat"
-gridlib.io.write_data_grid_resampling(path_save, fit_result_full, fit_results_resampled)
+    # Save the the resampled data
+    path_save = "examples/data/example1_resampling.mat"
+    gridlib.io.write_data_grid_resampling(
+        path_save, fit_result_full, fit_results_resampled
+    )
 
-# Load the resampled data (this is just for a sanity check)
-fit_result_full_2, fit_results_resampled_2 = gridlib.io.read_data_grid_resampling(
-    path_save
-)
+    # Load the resampled data (this is just for a sanity check)
+    fit_result_full_2, fit_results_resampled_2 = gridlib.io.read_data_grid_resampling(
+        path_save
+    )
 
-print(len(fit_result_full_2), len(fit_results_resampled_2))
+    print(len(fit_result_full_2), len(fit_results_resampled_2))
 
-# Plot the resampled data
-gridlib.plot.event_spectrum_heatmap(fit_result_full, fit_results_resampled)
-gridlib.plot.state_spectrum_heatmap(fit_result_full, fit_results_resampled)
+    # Plot the resampled data
+    gridlib.plot.event_spectrum_heatmap(fit_result_full, fit_results_resampled)
+    gridlib.plot.state_spectrum_heatmap(fit_result_full, fit_results_resampled)
 
-plt.show()
+    plt.show()
