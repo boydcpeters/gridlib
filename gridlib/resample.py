a57c0804d006a6e4452663663db4c41465ae8317
Boyd Peters <boyd.c.peters@gmail.com>
2022-11-30 18:00:40 +0100

    Updated function to allow for multiprocessing

--- gridlib/resample.py
+++ gridlib/resample.py
@@ -1,15 +1,19 @@
 from typing import Tuple, Dict, Union, List
 import math
 import time
+import concurrent.futures
+from concurrent.futures import ProcessPoolExecutor
 
 import numpy as np
+import psutil
+from tqdm import tqdm
 
 from . import compute
 from . import data_utils
 from . import fit
 
 # TODO: update docstring
-def _resample_data(data, perc: float = 0.8, seed=None):
+def _resample_data(data, perc: float = 0.8):
     """_summary_
 
     Parameters
@@ -18,19 +22,13 @@ def _resample_data(data, perc: float = 0.8, seed=None):
         _description_
     perc : float, optional
         _description_, by default 0.8
-    seed : {None, int, array_like[ints], SeedSequence, BitGenerator, Generator}, optional
-        A seed to initialize the BitGenerator. If None, then fresh, unpredictable
-        entropy will be pulled from the OS. If an int or array_like[ints] is passed,
-        then it will be passed to SeedSequence to derive the initial BitGenerator state.
-        One may also pass in a SeedSequence instance. Additionally, when passed a
-        BitGenerator, it will be wrapped by Generator. (THIS IS FROM NUMPY DOC, MAYBE CHANGE THIS), by default None
 
     Returns
     -------
     _type_
         _description_
     """
-    rng = np.random.default_rng(seed)
+    rng = np.random.default_rng()
 
     data_sampled = dict()
     for t_tl in data.keys():
@@ -89,12 +87,21 @@ def _resample_data(data, perc: float = 0.8, seed=None):
     return data_sampled
 
 
+def _resample_and_fit(parameters, data, perc: float = 0.8):
+    """Function that can be easily run with multiprocessing."""
+    data_resampled = _resample_data(data, perc=perc)
+
+    fit_results_temp = fit.fit_grid(parameters, data_resampled, disp=False)
+
+    return fit_results_temp
+
+
 def resampling_grid(
     parameters,
     data,
     n: int = 10,
     perc: float = 0.8,
-    seed=None,
+    multiprocess_flag: bool = True,
 ) -> Tuple[
     Dict[str, Union[np.ndarray, float]], List[Dict[str, Union[np.ndarray, float]]]
 ]:
@@ -122,23 +129,27 @@ def resampling_grid(
         _description_
     """
 
-    print("---------------------------------")
+    print("----------------------------------")
     print("Fitting of all data starts now...")
     # Determine the full GRID fit results based on all the data.
     fit_result_full = fit.fit_grid(parameters, data, disp=False)
     print("Fitting of all data finished now.")
-    print("---------------------------------")
+    print("----------------------------------")
     print("Start resampling...")
 
-    rng = np.random.default_rng(seed)
+    max_workers = 1
+    if multiprocess_flag:
+        # The number of logical cores minus 1, for the background tasks
+        max_workers = psutil.cpu_count(logical=False) - 1
 
     # Determine the GRID fit results for the resampled data
     fit_results_resampled = []
-    for i in range(n):
+
+    with tqdm(total=n) as pbar:
 
         t0 = time.time()
 
-        data_resampled = _resample_data(data, perc=perc, seed=rng)
+        data_resampled = _resample_data(data, perc=perc)
 
         fit_results_temp = fit.fit_grid(parameters, data_resampled, disp=False)
 
@@ -146,12 +157,39 @@ def resampling_grid(
 
         t1 = time.time()
 
-        if i == 0:
-            print(f"Estimated time: {((t1-t0)*n)/60.0:.0f} min")
+        print(f"Estimated time: {((t1-t0)*n)/60.0/max_workers:.0f} min")
 
-        print(f"{i}-th resampling is finished.")
+        pbar.update(1)
+        print()
+        print(f"1-th resampling is finished.")
 
-    print("Resampling is finished.")
-    print("---------------------------------")
+        if multiprocess_flag:
+            i = 2
+            with ProcessPoolExecutor(max_workers=max_workers) as executor:
+
+                # Submit the tasks
+                futures = [
+                    executor.submit(_resample_and_fit, parameters, data, perc)
+                    for _ in range(n - 1)
+                ]
+
+                # Check if a task is finished
+                for future in concurrent.futures.as_completed(futures):
+                    fit_results_temp = future.result()
+                    fit_results_resampled.append(fit_results_temp)
+                    pbar.update(1)
+                    print()
+                    print(f"{i}-th resampling is finished.")
+                    i += 1
+        else:
+            for i in range(2, n + 1):
+                fit_results_temp = _resample_and_fit(parameters, data, perc=perc)
+                fit_results_resampled.append(fit_results_temp)
+                pbar.update(1)
+                print()
+                print(f"{i}-th resampling is finished.")
+
+    print("Resampling is completely finished.")
+    print("----------------------------------")
 
     return fit_result_full, fit_results_resampled
